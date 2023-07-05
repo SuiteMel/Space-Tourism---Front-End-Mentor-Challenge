d3e952099ef90e2b4225a5d5e6276d532d7e870f
SuiteMel <melissamwaltner@gmail.com>
2023-07-05 13:43:23 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="text-center flex flex-col justify-center h-screen">
+  <div class="flex justify-center">
+    <a class="inline-block" href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a class="inline-block" href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1 class="text-5xl font-bold mb-5">Vite + Svelte</h1>
+
+  <div class="card mb-8">
+    <Counter />
+  </div>
+
+  <p class="mb-5">
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6rem;
+    padding: 1.5rem;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button class="text-red-500 p-2 rounded" on:click={increment}>
+  count is {count}
+</button>
+
+<style lang="postcss">
+  button {
+    background-color: theme('colors.gray.300');
+  }
+</style>
