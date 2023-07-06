1c5efe57bea3fd013672381544ab8eb5992c5a5d
Melissa <melissamwaltner@gmail.com>
2023-07-06 12:56:12 -0500

    Home page layout

---
+++ src/pages/home.svelte
@@ -0,0 +1,33 @@
+<script>
+  import bgDesktop from '../assets/home/background-home-desktop.jpg'
+  import bgTablet from '../assets/home/background-home-tablet.jpg'
+  import bgMobile from '../assets/home/background-home-mobile.jpg'
+</script>
+
+<main class="relative">
+  <picture class="absolute inset-0">
+    <source srcset="{bgDesktop}" media="(min-width: 1024px)">
+    <source srcset="{bgTablet}" media="(min-width: 768px)">
+    <img class="h-full w-full object-cover" src="{bgMobile}" alt="Space View of Earth">
+  </picture>
+
+  <div class="container mx-auto flex lg:justify-around items-center lg:items-end z-0 relative min-h-screen lg:pb-32 lg:flex-row flex-col justify-around text-center lg:text-left">
+    <div class="w-min">
+      <h2 class="uppercase text-xl/tight text-secondary mb-6 tracking-widest">
+        So, you want to travel to
+      </h2>
+    
+      <h1 class="uppercase font-serif text-5xl/none">
+        Space
+      </h1>
+    
+      <p class="text-lg text-secondary mt-6">
+        Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+      </p>
+    </div>
+
+    <a href="/" class="inline-flex justify-center items-center font-serif bg-white text-primary uppercase aspect-square rounded-full text-2xl px-16 tracking-wider hover:outline outline-[5.5rem] outline-white/10">
+      Explore
+    </a>
+  </div>
+</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 export default {
   content: ['./index.html', './src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0B0D17',
+        secondary: '#D0D6F9',
+      },
+      fontFamily: {
+        sans: [
+          '"Barlow Condensed"',
+          'sans-serif'
+        ],
+        serif: [
+          'Bellefair',
+          'serif'
+        ]
+      },
+      fontSize: {
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.75rem',
+        '2xl': '2rem',
+        '3xl': '3.5rem',
+        '4xl': '6.25rem',
+        '5xl': '9.375rem'
+      }
+    },
   },
   plugins: [],
 };
+
+/*
+  9.375rem
+  6.25rem
+  3.5rem
+  2rem
+  1.75rem
+  1.125rem
+  1rem
+  0.875rem
+*/
